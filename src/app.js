02232b97a688eba519657f8361312efe015f2b36
Bilge Kaan Boluk <k@kaan.io>
2025-05-23 01:15:57 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const loginForm = document.querySelector('form#login')
+let socket
+
+const connect = () => {
+  // Create WebSocket connection.
+  socket = new WebSocket("ws://localhost:8080");
+
+  // Connection opened
+  socket.addEventListener("open", (event) => {
+    console.log("Hello Server!");
+  });
+
+  // Listen for messages
+  socket.addEventListener("message", (event) => {
+    console.log("Message from server ", event.data);
+  });
+
+}
+
+loginForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const formData = new FormData(loginForm)
+  const name = formData.get('name')
+  fetch('http://localhost:8080/login', { method: 'POST', body: JSON.stringify({ name }), headers: { 'content-type': 'application/json' } })
+    .then(i => i.json())
+    .then(connect)
+})
